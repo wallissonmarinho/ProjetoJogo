8f9ff9c471db36c76b2216c8512a0b37a2265a7b
WallissonMarinho <wallissonmarinho@hotmail.com>
2019-11-24 11:07:05 -0300

    subindo projeto

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //public int vida;
+    //public string nome;
+
+    //public float tempo;
+    //public float distancia;
+    //public float velocidade; 
+    
+    public float forcaPulo;
+    public float vMaxima;
+    private float movimento;
+    
+
+    void Start()
+    {
+        //VerificaVelocidade();
+    }
+
+
+    void Update()
+    {
+        Movimentos();
+    }
+
+    void Movimentos()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+
+        movimento = Input.GetAxis("Horizontal");
+        
+        //Função para movimentar para esquerda e para direita
+        rigidbody.velocity = new Vector2(movimento*vMaxima, rigidbody.velocity.y);
+
+        //Função para flipar a imagem
+        if(movimento < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }else if(movimento > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        //Função para pular
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0, forcaPulo));
+            
+        }
+    }
+
+}
