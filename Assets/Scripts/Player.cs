ce42b449fcde788b57f27771d6741f876869eced
WallissonMarinho <wallissonmarinho@hotmail.com>
2019-11-24 13:57:17 -0300

    animation adicionado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,7 +32,7 @@ public class Player : MonoBehaviour
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
         movimento = Input.GetAxis("Horizontal");
-        
+
         //Função para movimentar para esquerda e para direita
         rigidbody.velocity = new Vector2(movimento*vMaxima, rigidbody.velocity.y);
 
@@ -43,6 +43,14 @@ public class Player : MonoBehaviour
         }else if(movimento > 0)
         {
             GetComponent<SpriteRenderer>().flipX = false;
+        }
+        //Transição de Idle para Run
+        if(movimento > 0 || movimento <0)
+        {
+            GetComponent<Animator>().SetBool("Walking", true);
+        }else
+        {
+            GetComponent<Animator>().SetBool("Walking", false);
         }
         //Função para pular
         if(Input.GetKeyDown(KeyCode.Space))
