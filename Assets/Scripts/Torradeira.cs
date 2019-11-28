3e9694226687be7e48b9db98b20ec1263d0037f7
WallissonMarinho <wallissonmarinho@hotmail.com>
2019-11-28 19:26:23 -0300

    script torradeira adicionado

---
+++ Assets/Scripts/Torradeira.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torradeira : MonoBehaviour
+{
+    private Animator Torrad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Torrad = GetComponent<Animator>();
+        Torrad.SetBool("Presenca",true);
+        
+    }
+}
